035fc8554e494d9e0490057ae323490f2c7af6a3
OriiaC <edison_agudelo@hotmail.com>
2023-02-20 20:05:40 -0500

    se agregan las soluciones de los ejercicios del modulo 03

---
+++ Curso preparatorio henry/Variables.js
@@ -0,0 +1,13 @@
+//String
+var cajon = "bufanda verde";
+//numeros
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+//Booleanos
+var estoyAprendiendo=true;
+var estoyAburrido=false;
+//undefined
+var cajon;
+//null
+var sinNada = null;
---
+++ Curso preparatorio henry/funciones.js
@@ -0,0 +1,14 @@
+//PRIMERO
+function sumaTres(x){
+    return x+3;
+}
+
+//SEGUNDO
+var sumaTres = function (x){
+    return x+3;
+}
+
+//TERSERO
+var sumaTres = (x) => {
+    return x+3;
+}

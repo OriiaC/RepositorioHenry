8186e3b2fd0cb9db622aeceaca211eb49ae74005
OriiaC <edison_agudelo@hotmail.com>
2023-02-21 11:12:14 -0500

    se agrega un documento de pruebas

---
+++ Curso preparatorio henry/pruebas.js
@@ -0,0 +1,9 @@
+function tieneTresDigitos(num) {
+    // Si el número recibido tiene tres dígitos retornar true.
+    // Caso contrario, retornar false.
+    // Tu código:
+    str = num.toString();
+    console.log(str);
+ }
+
+ tieneTresDigitos(1231)
